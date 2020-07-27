b401ec0283e53f9ea4af9a7453f6b89e480e5e94
siddarthagadamsetty <siddarthrahul@gmail.com>
2020-07-27 17:38:29 +0530

    simplified the code

--- LeetCode/src/LinkedList/LinkedListCycle_I.java
+++ LeetCode/src/LinkedList/LinkedListCycle_I.java
@@ -50,7 +50,24 @@ public class LinkedListCycle_I {
 
 	}
 	
-public boolean hasCycle(ListNode head) {
+	 public boolean hasCycle(ListNode head) {
+	        
+	        if (head == null || head.next == null) {
+	            return false;
+	        }
+	        
+	        ListNode slow = head;
+	        ListNode fast = head;
+	        
+	        while (fast != null && fast.next != null) { 
+	            slow = slow.next;
+	            fast = fast.next.next;
+	            if (slow == fast) return true;
+	        }
+	        return false;
+	    }
+	
+public boolean hasCycle1(ListNode head) {
         
         ListNode slowPointer = head;
         ListNode fastPointer = head;
